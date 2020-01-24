64b29992e39dcba41a0c43e65dd1d63336c27f86
Ethan Davidson <theejdavidson@gmail.com>
2020-01-24 21:44:16 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,8 @@ def find_element_index(array, value_to_find)
   array.length.times { |i|
     if(array[i] == value_to_find)
       return i
-    else return nil
+    else
+      return nil
     end
   }
 end
